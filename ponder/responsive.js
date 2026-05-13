5fc657e0b4a1ca4075c63f30b1ef7ef06e8d9f7f
PMOD-77 <pmod77@byui.edu>
2026-05-13 16:25:01 -0600

    letsgo!

--- ponder/responsive.js
+++ ponder/responsive.js
@@ -7,3 +7,11 @@ function toggleMenu() {
     menu.classList.toggle('hide');
     btn.classList.toggle('change');
 }
+
+let password = '123test';
+let username = 'user_testorian123';
+
+// if 
+//  (username === 'user_testorian123' && password === '123test') console.log('Welcome, user_testorian123!');
+// else 
+//  console.log('Invalid username or password. Please try again.');
